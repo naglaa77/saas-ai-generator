9db768e19dea9e9417e9532b6fa550c4f8359dcb
naglaa77 <naglaafouz4@gmail.com>
2024-04-22 10:48:04 +0200

    Feat(Implement): Finish conversion ai ui

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@/components/sidebar";
 
 export default function DashboardLayout({children}:{children:React.ReactNode }){
     return (
-        <div className="h-full relative bg-amber-300">
+        <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
                <Sidebar />
             </div>
