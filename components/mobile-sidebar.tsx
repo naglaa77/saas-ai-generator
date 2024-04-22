5bf6774339aac6bac06f85bac084bb8c8ff985c7
naglaa77 <naglaafouz4@gmail.com>
2024-04-22 08:36:24 +0200

    Feat(Implement): Implement dashboard page

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,18 @@ import {Button} from "@/components/ui/button";
 import {Menu, } from "lucide-react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import {useEffect, useState} from "react";
 
 export default function MobileSidebar() {
+// i do that because i have error Hydration where In HTML, <button> cannot be a descendant of <button>. so
+// i use this to prevent the error useState and useEffect
+    const [isMounted, setIsMounted] = useState(false)
+    useEffect(() => {
+        setIsMounted(true)
+    },[])
+    if(!isMounted){
+        return null
+    }
     return (
        <Sheet>
            <SheetTrigger>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,10 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
 import {Montserrat} from "next/font/google";
 import {cn} from "@/lib/utils";
 import {Code, ImagesIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon} from "lucide-react";
+import {usePathname} from "next/navigation";
 
 
 const montserrat = Montserrat({
@@ -54,6 +56,10 @@ const routes = [
     }
 ]
 export default function Sidebar() {
+
+   const pathName =  usePathname()
+    console.log("pathName",pathName)
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -65,8 +71,9 @@ export default function Sidebar() {
                 </Link>
                 <div className="space-y-1">
                     {routes.map(route => (
-                        <Link href={route.href} key={route.href} className="text-sm flex group p-3 w-full justify-start
-                         font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                        <Link href={route.href} key={route.href} className={cn("text-sm flex group p-3 w-full" +
+                            " justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg" +
+                            " transition",pathName === route.href? "text-white bg-white/10" :"text-zinc-400")}>
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("w-5 h-5 mr-3",route.color)}/>
                                 {route.label}
